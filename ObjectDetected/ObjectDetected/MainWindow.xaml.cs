96233719343c2a273fc29fccc82bbd7e1de30022
Senri12 <97281809+Senri12@users.noreply.github.com>
2024-01-14 20:10:34 +0300

    Add files via upload

---
+++ ObjectDetected/ObjectDetected/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObjectDetected
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Log_Click(object sender, RoutedEventArgs e)
+        {
+            // Здесь вы можете добавить логику проверки логина и пароля,
+            // например, сравнение с данными в базе данных.
+
+            MessageBox.Show("Авторизация прошла успешно!");
+            Window2 window2 = new Window2();
+            // Показываем окно Window1
+            window2.Show();
+            this.Close();
+            
+        }
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            // В этом месте вы можете обработать данные регистрации, например, отправив их на сервер.
+
+            MessageBox.Show("Пользователь успешно зарегистрирован!");
+            Window1 window1 = new Window1();
+            // Показываем окно Window1
+            window1.Show();
+
+            this.Close();
+        }
+    }
+}
