3516496ff02ef9bb6adf50f5dc4e841ed4c72dd6
Senri <shaburov8242@gmail.com>
2024-01-16 19:30:02 +0300

    Перемотка видео

--- ObjectDetected/ObjectDetected/Window3.xaml.cs
+++ ObjectDetected/ObjectDetected/Window3.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,12 +20,17 @@ namespace ObjectDetected
     /// </summary>
     public partial class Window3 : Window
     {
+        private System.Windows.Threading.DispatcherTimer positionTimer;
+        private bool isDraggingSlider = false;
         public Window3()
         {
             InitializeComponent();
         }
         private void Play_Click(object sender, RoutedEventArgs e)
         {
+            positionTimer.Start();
+            isDraggingSlider = false;
+
             mediaPlayer.Play();
         }
 
@@ -49,10 +55,64 @@ namespace ObjectDetected
             {
                 mediaPlayer.Source = new Uri(videoPath);
                 mediaPlayer.Play();
+                mediaPlayer.MediaOpened += MediaElement_MediaOpened;
+                positionTimer = new System.Windows.Threading.DispatcherTimer();
+                positionTimer.Interval = TimeSpan.FromMilliseconds(100);
+                positionTimer.Tick += PositionTimer_Tick;
+                mediaPlayer.MediaEnded += MediaElement_MediaEnded;
+                progressSlider.AddHandler(Thumb.DragStartedEvent, new DragStartedEventHandler(Slider_DragStarted));
+                progressSlider.AddHandler(Thumb.DragCompletedEvent, new DragCompletedEventHandler(Slider_DragCompleted));
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Ошибка при загрузке видео: {ex.Message}");
+            }
+        }
+        private void MediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            // Обновление интерфейса, например, обновление продолжительности ProgressBar
+            progressSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+            positionTimer.Start();
+            isDraggingSlider = false;
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            isDraggingSlider = true;
+            // Сброс времени воспроизведения и слайдера по завершении видео
+            mediaPlayer.Stop();
+            progressSlider.Value = 0;
+
+            // Остановка таймера при завершении медиа
+            positionTimer.Stop();
+        }
+
+        // Подписка на событие изменения времени воспроизведения во время воспроизведения
+        private void PositionTimer_Tick(object sender, EventArgs e)
+        {
+            // Обновление положения слайдера при изменении времени воспроизведения
+            if (!isDraggingSlider)
+            {
+                progressSlider.Value = mediaPlayer.Position.TotalSeconds;
+            }
+        }
+        private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            isDraggingSlider = true;
+            mediaPlayer.Pause();
+        }
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            isDraggingSlider = false;
+            mediaPlayer.Position = TimeSpan.FromSeconds(progressSlider.Value);
+            mediaPlayer.Play();
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Обновление времени воспроизведения при изменении положения слайдера
+            if (!isDraggingSlider)
+            {
+                mediaPlayer.Position = TimeSpan.FromSeconds(progressSlider.Value);
             }
         }
     }
