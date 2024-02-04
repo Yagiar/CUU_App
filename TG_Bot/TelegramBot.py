8a7045ee2ede2276cea0344bff5473d9e8c1818b
amv <amv.21@uni-dubna.ru>
2024-02-04 16:49:32 +0300

    /send_destruction вместо /send_image; теперь пишется количество изображений в папке

--- TG_Bot/TelegramBot.py
+++ TG_Bot/TelegramBot.py
@@ -10,6 +10,7 @@ def send_image(update: Update, context: CallbackContext) -> None:
     image_folder_path = "../ObjectDetected/ObjectDetected/destruction"
     # Получаем список файлов в папке
     image_files = [f for f in os.listdir(image_folder_path) if os.path.isfile(os.path.join(image_folder_path, f))]
+    update.message.reply_text('В папке {} изображений'.format(len(image_files)))
     # Выбираем первое изображение из списка (вы можете выбрать любой другой способ выбора)
     if image_files:
         for image in image_files:
@@ -23,7 +24,7 @@ def main() -> None:
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("send_image", send_image))  # Добавляем команду для отправки изображения
+    dp.add_handler(CommandHandler("send_destruction", send_image))  # Добавляем команду для отправки изображения
 
     updater.start_polling()
     updater.idle()
